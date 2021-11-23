f1268d0764e2e3ff78659250f6dba8b3b19d425d
Makdoom Shaikh <makshaikh99@gmail.com>
2021-11-23 15:17:27 +0530

    Added header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      {/* Home */}
-      <Home />
-
-      {/* Search Page */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<h1> search </h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
 import "./home.css";
+import { Avatar } from "@material-ui/core";
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home compo</h1>
+      <div className="home__header">
+        <div className="header__left">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
+        <div className="header__right">
+          <Link to="/about">Gmail</Link>
+          <Link to="/store">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </div>
+      </div>
     </div>
   );
 };
